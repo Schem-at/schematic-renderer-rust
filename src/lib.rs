8eb137807680615cf7523ce3be254d195f6f132c
cmoafr <cmoa58@gmail.com>
2024-08-19 23:35:44 +0200

    Change to Bevy

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,34 @@
+use bevy::{prelude::*, window::WindowMode};
 use wasm_bindgen::prelude::*;
 
+// Entry point for the WebAssembly application
 #[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
+pub fn run() {
+    let plugins = DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            canvas: Some("#renderCanvas".into()),
+            mode: WindowMode::Fullscreen, // Ensures that it fills the canvas without resizing
+            ..default()
+        }),
+        ..default()
+    });
+    App::new()
+        .insert_resource(ClearColor(Color::WHITE))
+        .add_plugins(plugins)
+        .add_systems(Startup, setup)
+        .run();
 }
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+
+    // TEMP: Red rectangle
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::srgb(1.0, 0.0, 0.0),
+            custom_size: Some(Vec2::new(200.0, 100.0)), // width, height
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
